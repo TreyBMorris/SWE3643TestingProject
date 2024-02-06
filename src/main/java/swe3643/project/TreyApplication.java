8aab45b47192cbbdb508f9d794f8760aa0bcd91a
Trey <trey@morris2morris.com>
2024-02-06 18:56:34 -0500

    Initial Commit

---
+++ src/main/java/swe3643/project/TreyApplication.java
@@ -0,0 +1,13 @@
+package swe3643.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swe3643/project/TreyApplicationTests.java
@@ -0,0 +1,13 @@
+package swe3643.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
