0e9d97b40658c9b8f8c7916dd92e1c6218ab5f7c
Trey <trey@morris2morris.com>
2024-02-18 18:09:31 -0500

    Add Methods and CalculationResult Class
    - Added Multiply and Divide Methods
    - Added Multiply Tests
    - Added Divide Tests, including divide by zero throws error
    - Added CalculatorResult Class

--- src/calculator-engine/src/main/java/CalculatorEngine.java
+++ src/calculator-engine/src/main/java/CalculatorEngine.java
@@ -5,11 +5,56 @@ public class CalculatorEngine
 
     public double add(double firstNumber, double secondNumber)
     {
-        return firstNumber + secondNumber;
+        CalculationResult addResult = new CalculationResult();
+        addResult.setSuccess(true);
+        addResult.setResult(firstNumber+secondNumber);
+        addResult.setOperation(firstNumber + "+" + secondNumber);
+        addResult.setError("");
+
+        return addResult.getResult();
     }
 
     public double subtract(double firstNumber, double secondNumber)
     {
-        return firstNumber - secondNumber;
+        CalculationResult subtractResult = new CalculationResult();
+        subtractResult.setSuccess(true);
+        subtractResult.setResult(firstNumber-secondNumber);
+        subtractResult.setOperation(firstNumber + "-" + secondNumber);
+        subtractResult.setError("");
+        return subtractResult.getResult();
+    }
+
+    public double multiplication(double firstNumber, double secondNumber)
+    {
+        CalculationResult multiplyResult = new CalculationResult();
+        multiplyResult.setSuccess(true);
+        multiplyResult.setResult(firstNumber*secondNumber);
+        multiplyResult.setOperation(firstNumber + "*" + secondNumber);
+        multiplyResult.setError("");
+        return multiplyResult.getResult();
+    }
+
+    public static CalculationResult divide(double firstNumber, double secondNumber)
+    {
+        CalculationResult result = new CalculationResult();
+        result.setOperation(firstNumber + " / " + secondNumber + " =");
+
+        try
+        {
+            if(secondNumber == 0)
+            {
+                throw new DivisionByZeroException("Division by Zero is not allowed");
+            }
+            result.setResult(firstNumber/secondNumber);
+            result.setSuccess(true);
+            result.setError("");
+        }
+        catch(DivisionByZeroException e)
+        {
+            result.setResult(0.0);
+            result.setSuccess(false);
+            result.setError(e.getMessage());
+        }
+        return result;
     }
 }
--- src/tests/src/main/java/CalculatorEngineTests.java
+++ src/tests/src/main/java/CalculatorEngineTests.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestExecutionListeners;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class CalculatorEngineTests
 {
@@ -32,4 +34,47 @@ public class CalculatorEngineTests
         //Assert
         assertEquals(result, expected);
     }
+
+    @Test
+    public void Multiply_TwoFloatingPointValues_ReturnsProduct()
+    {
+        CalculatorEngine engine = new CalculatorEngine();
+        double firstNum = 5;
+        double secondNum = 7.1;
+        double expected = 35.5;
+
+        double result = engine.multiplication(firstNum,secondNum);
+
+        assertEquals(result,expected);
+    }
+
+    @Test
+    public void Divide_TwoFloatingPointValues_ReturnsQuotient()
+    {
+        double firstNum = 3.0;
+        double secondNum = 9.0;
+        double expected = 3.0/9.0;
+
+        CalculationResult result = CalculatorEngine.divide(firstNum,secondNum);
+
+
+
+        assertEquals(result.getResult(),expected);
+    }
+
+    @Test
+    public void Divide_ByZero_ThrowsError()
+    {
+        double firstNum = 3.0;
+        double secondNum = 0.0;
+        boolean isSuccess = false;
+        String error = "Division by Zero is not allowed";
+        CalculationResult result = CalculatorEngine.divide(firstNum,secondNum);
+
+        assertFalse(result.isSuccess());
+        assertEquals(0.0,result.getResult(),0.0001);
+        assertEquals(error,result.getError());
+
+    }
+
 }
